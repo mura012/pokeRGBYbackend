23a3166fd8fa031d0339af66d242c3a0f1f9253d
github user mura012 <murakoudai@gmail.com>
2023-01-08 21:03:58 +0900

    re

--- api-routes/index.mjs
+++ api-routes/index.mjs
@@ -2,6 +2,7 @@ import pokemonRouter from "./pokemon.mjs";
 import express from "express";
 
 const router = express.Router();
+console.log("test");
 router.use("/pokemon", pokemonRouter);
 
 export default router;
