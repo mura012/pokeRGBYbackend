2493470bc3d66336c9aeb4dc75ba361d47540193
github user mura012 <murakoudai@gmail.com>
2023-01-08 21:41:24 +0900

    cors追加

--- app.mjs
+++ app.mjs
@@ -2,10 +2,16 @@ import express from "express";
 import apiRoutes from "./api-routes/index.mjs";
 import env from "dotenv";
 import "./helpers/db.mjs";
+import cors from "cors";
 env.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(
+  cors({
+    origin: "https://pokemonredgreentools-f2v9v5pz4-mura012.vercel.app",
+  })
+);
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
