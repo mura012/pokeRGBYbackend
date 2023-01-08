75b9d3f309cdcfd6434542c3a73c4d6a760c14f7
github user mura012 <murakoudai@gmail.com>
2023-01-09 08:25:38 +0900

    変更

--- app.mjs
+++ app.mjs
@@ -3,13 +3,14 @@ import apiRoutes from "./api-routes/index.mjs";
 import env from "dotenv";
 import "./helpers/db.mjs";
 import cors from "cors";
+
 env.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(
   cors({
-    origin: "https://pokemonredgreentools-f2v9v5pz4-mura012.vercel.app",
+    origin: process.env.POKEMON_API_KEY,
   })
 );
 app.get("/", (req, res) => {
